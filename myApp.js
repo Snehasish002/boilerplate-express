d841341c98ecf9148173af243aadca819675100e
Snehasish Mohanty <snehasishmohanty9439@gmail.com>
2024-09-15 04:14:52 +0000

    Recreate-myApp-comp

--- myApp.js
+++ myApp.js
@@ -1,9 +1,42 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+// #1
+// console.log("Hello World")
 
+//#2
+// app.get("/",(req,res) => {
+//     res.send("Hello Express")
+// })
 
+//#4
+app.use("/public", express.static(__dirname + "/public"))
+
+//#3
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "views/index.html");
+})
+
+//#5
+// app.get("/json", (req,res) => {
+//     res.json({
+//         "message": "Hello json"
+//     })
+// })
+
+//#6
+
+app.get("/json", (req,res) => {
+    if( process.env["MESSAGE_STYLE"] == "uppercase"){
+        res.json({
+            "message":"HELLO JSON"
+        })
+    }else{
+        res.json({
+            "message":"Hello json"
+        })
+    }
+})
 
 
 
